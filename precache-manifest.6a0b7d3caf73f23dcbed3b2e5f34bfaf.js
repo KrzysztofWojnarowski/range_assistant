b5edb63733de1b02a80d96e103c576f38bed81c7
Krzysztof Wojnarowski <the.one.and.only.krzysztof@gmail.com>
2024-01-29 20:32:55 +0100

    Updates

---
+++ precache-manifest.6a0b7d3caf73f23dcbed3b2e5f34bfaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b821f7895611ed7e091b05feb488737c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c61aaae27a87e0ab541",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "3475b344cdc3960f3b39",
+    "url": "/static/js/2.e1a696e6.chunk.js"
+  },
+  {
+    "revision": "7e1ca1c667044397f017c017ea2dc59a",
+    "url": "/static/js/2.e1a696e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ee786df05d49124348",
+    "url": "/static/js/3.e9878a3a.chunk.js"
+  },
+  {
+    "revision": "2c61aaae27a87e0ab541",
+    "url": "/static/js/main.42ecac6b.chunk.js"
+  },
+  {
+    "revision": "64cef59e25ed54879603",
+    "url": "/static/js/runtime-main.1d6f9ee7.js"
+  }
+]);
